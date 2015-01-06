95db424282b971a3c3a9272060c972e92c93457d
jamesrichford <jamesrichford@googlemail.com>
2015-01-06 22:24:47 +0000

    Pattern Builder: Initialise grid

--- Imaginarium GUI/pattern-builder/js/pattern-builder.js
+++ Imaginarium GUI/pattern-builder/js/pattern-builder.js
@@ -1,1 +1,17 @@
-﻿
+﻿function createRectangle(x, y) {
+    var el = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    el.setAttribute('x', x);
+    el.setAttribute('y', y);
+    el.setAttribute('height', 10);
+    el.setAttribute('width', 10);
+    el.setAttribute('fill', 'rgb(0,0,0)');
+    document.getElementById('pattern-canvas').appendChild(el);
+}
+
+for (var i = 0; i < 10; i++)
+{
+    for (var j = 0; j < 10; j++)
+    {
+        createRectangle(15 * j + 10, 15 * i + 10);
+    }
+}
