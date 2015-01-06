24c39a075567e9402ef9ba315afb7345a4bc3691
jamesrichford <jamesrichford@googlemail.com>
2015-01-06 21:33:49 +0000

    Initial commit

---
+++ Imaginarium GUI/pattern-builder/js/app.js
@@ -0,0 +1,18 @@
+﻿if (apiUrl === undefined) {
+    var apiUrl = 'http://api.getsett.net';
+}
+
+if (app === undefined) {
+    var app = angular.module('sett-site', ['ngMaterial']);
+}
+app.controller('side-nav-controller', function ($scope, $mdSidenav) {
+    $scope.openLeftMenu = function () {
+        $mdSidenav('left').toggle();
+    };
+});
+
+app.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+      .primaryColor('purple')
+      .accentColor('green');
+});
