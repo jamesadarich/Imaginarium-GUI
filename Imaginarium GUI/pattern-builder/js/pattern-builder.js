390d82e633763bbbd026ac3f329756560995de03
jamesrichford <jamesrichford@googlemail.com>
2015-01-09 20:20:46 +0000

    Pattern Builder: resize pattern canvas

--- Imaginarium GUI/pattern-builder/js/pattern-builder.js
+++ Imaginarium GUI/pattern-builder/js/pattern-builder.js
@@ -1,25 +1,7 @@
-﻿function createRectangle(x, y) {
-    var el = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-    el.setAttribute('x', x);
-    el.setAttribute('y', y);
-    el.setAttribute('height', 20);
-    el.setAttribute('width', 20);
-    el.setAttribute('fill', 'rgb(0,0,0)');
-    $(el).click(function () {
-        el.setAttribute('fill', 'rgb(255,0,0)');
-    });
-    document.getElementById('pattern-canvas').appendChild(el);
-}
+﻿$(window).load(function () {
 
-/*
-$(document).ready(function () {
-    for (var i = 0; i < 10; i++) {
-        for (var j = 0; j < 10; j++) {
-            createRectangle(25 * j + 10, 25 * i + 10);
-        }
-    }
+    $('#pattern-canvas').height($(document).height() - $('#main-tool-bar').height());
 });
-*/
 
 if (app === undefined) {
     var app = angular.module('pattern-builder', ['ngMaterial']);
@@ -134,6 +116,7 @@ app.controller('pattern-builder-controller', function ($scope, $mdDialog) {
     });
 
     $scope.toggleColor = function (square) {
+
         if (square.color == $scope.selectedColor) {
             square.color = $scope.defaultColor;
         }
