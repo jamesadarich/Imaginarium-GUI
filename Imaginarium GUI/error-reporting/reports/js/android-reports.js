58c5635dfe326e18113c08dfcf088bc4e7756914
jamesrichford <jamesrichford@googlemail.com>
2015-01-21 20:42:33 +0000

    Add error reporting

---
+++ Imaginarium GUI/error-reporting/reports/js/android-reports.js
@@ -0,0 +1,14 @@
+﻿if (app === undefined) {
+    var app = angular.module('android-reports', ['ngMaterial']);
+}
+
+app.controller('android-reports-controller', function ($scope, $mdDialog) {
+
+    $.get('http://api.imaginarium.getsett.net/error-reporting/reports/android')
+    .success(function (reports) {
+        $scope.reports = reports;
+    });
+
+    $scope.deleteReport = function (report) {
+    } 
+});
