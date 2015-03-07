aabceed3bd5e4913e34d75ba323f541209bb8dd7
jamesrichford <jamesrichford@googlemail.com>
2015-03-07 23:56:35 +0000

    LoL view reviews

---
+++ Imaginarium GUI/legends-of-lunchtime/controllers/review-summary.js
@@ -0,0 +1,7 @@
+﻿app.controller('review-summary-controller', function ($scope, $mdDialog) {
+
+    $.get('http://api.imaginarium.getsett.net/legends-of-lunchtime/reviews')
+    .success(function (reviews) {
+        $scope.reviews = reviews;
+    });
+});
